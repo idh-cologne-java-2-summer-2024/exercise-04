4073f0616d5887a5b708725ea2cc00a380a7cd45
pauls <pauls@172.29.242.23>
2024-05-11 16:37:35 +0200

    Aufgabe 1

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,12 +2,14 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.Random;
 
 public class ATM {
 
     // initial cash in the ATM
     int cash = 100;
+    int nextPosition = 0;
 
     // accounts known to the ATM
     Account[] accounts = new Account[5];
@@ -19,28 +21,42 @@ public class ATM {
 	    accounts[i] = new Account(i, random.nextInt(1000));
 	}
     }
-
+    //this.accounts = new Account(nextPosition);
+    
+public void addAccount() throws Exception{
+	if(nextPosition < accounts.length) {
+		Random random = new Random();
+		accounts[nextPosition++] = new Account(nextPosition,random.nextInt(1000) );
+	}else
+	    throw new Exception("Accountsystem full");
+    
+}
     /**
      * Main command loop of the ATM Asks the user to enter a number, and passes this
      * number to the function cashout(...) which actually does the calculation and
      * produces money. If the user enters anything else than an integer number, the
      * loop breaks and the program exists
      */
-    public void run() {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public void run(ATM atm) {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	while (true) {
+		AccountIterator iter = new AccountIterator(atm);
 	    try {
+	    while(iter.hasNext()) {
+	    	
 		System.out.print("Enter your account number: ");
 		int accountNumber = Integer.parseInt(br.readLine());
 		System.out.print("Enter the amount to withdraw: ");
 		int amount = Integer.parseInt(br.readLine());
 		cashout(accountNumber, amount);
-	    } catch (Exception e) {
+	    } }
+	    catch (Exception e) {
 		e.printStackTrace();
 		break;
-	    }
+	    }}
 	}
-    }
+    
 
     public void cashout(int accountNumber, int amount) {
 	// check for cash in the ATM
@@ -74,7 +90,8 @@ public class ATM {
      */
     public static void main(String[] args) {
 	ATM atm = new ATM();
-	atm.run();
+	
+	atm.run(atm);
     };
 
     /**
@@ -91,4 +108,11 @@ public class ATM {
 	return null;
     }
 
+    	public int getSize() {
+    		return accounts.length;
+    	}
+    	
+   public Iterator<Account>	 iterator() {
+	   return new AccountIterator(this);
+   }
 }
---
+++ src/idh/java/AccountIterator.java
@@ -0,0 +1,27 @@
+package idh.java;
+
+import java.util.Iterator;
+
+public class AccountIterator implements Iterator<Account> {
+
+	ATM atm;
+	int currentPosition = -1; // i
+	
+	public AccountIterator(ATM atm) {
+		this.atm = atm;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		
+		return currentPosition < atm.getSize()-1;
+	}
+
+	@Override
+	public Account next() {
+		currentPosition++;
+		
+		return atm.getAccount(currentPosition);
+	}
+
+}
