7052cae049c416c75e3676c6a7cd4d4dd8380e64
loki <loki@192.168.8.137>
2024-05-14 20:13:28 +0200

    aufgabe 2, jetzt verstanden. Aber das pushen ist ger größte pain, warum klappt es manchmal einfach nciht?!!!!

--- src/idh/java/Bank1.java
+++ src/idh/java/Bank1.java
@@ -1,5 +1,21 @@
 package idh.java;
 
-public class Bank1 {
+import java.util.Iterator;
 
-}
+public class Bank implements Iterable<Account> {
+	private Account[] accounts;
+
+	public Bank(int size) {
+		accounts = new Account[size];
+	}
+
+	public Bank() {
+		accounts = new Account[50];
+	}
+
+	public void init() {
+		for (int i = 0; i < accounts.length; i++) {
+			accounts[i] = new Account(i, 50);}}
+
+	public Iterator<Account> iterator() {
+		return new AccountIterator(accounts);}}
